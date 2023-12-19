ffe41d617a7c63d238a013f4565f02848a450642
Divyanshu Shukla <divyanshushukla82@gmail.com>
2023-12-19 14:15:10 +0530

    Add files via upload

---
+++ Backend/Helpers/database/connectDatabase.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const connectDatabase =async  () => {
+
+    await mongoose.connect(process.env.MONGO_URI)
+
+    console.log("MongoDB Connection Successfully")
+
+}
+
+module.exports = connectDatabase
